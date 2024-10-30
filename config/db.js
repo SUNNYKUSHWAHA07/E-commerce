f046a693e8d33d38114554970d284df346008a10
SUNNY KUSHAWAHA <96289000+SUNNYKUSHWAHA07@users.noreply.github.com>
2024-10-30 13:59:15 +0530

    some minor changes

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
-
-
-mongoose.connect(process.env.MONGOURL,{
+mongoose.connect(process.env.MONGOURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 30000,
     bufferCommands: false, 
-}).then(function(){
+}).then(function() {
     console.log("connected to mongoDB");
-})
+}).catch(function(err) {
+    console.error("MongoDB connection error:", err);
+});
 
-module.exports = mongoose.connection
+module.exports = mongoose.connection;
--- config/google_auth_config.js
+++ config/google_auth_config.js
@@ -8,7 +8,7 @@ passport.use(new GoogleStrategy(
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.NODE_ENV === 'production' 
-    ? "https://e-commerce-722w.onrender.com/auth/google/callback" 
+    ? process.env.GOOGLE_CALLBACK_URL  
     : "http://localhost:4000/auth/google/callback",
   },
   async function(accessToken, refreshToken, profile, cb) {
