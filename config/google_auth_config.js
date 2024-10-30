f1ac8e43540b255caec75b40819ca8eef5082f4c
SUNNY KUSHAWAHA <96289000+SUNNYKUSHWAHA07@users.noreply.github.com>
2024-10-30 12:38:34 +0530

    excahnged the places

--- config/google_auth_config.js
+++ config/google_auth_config.js
@@ -7,7 +7,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"http://localhost:4000/auth/google/callback"||"https://e-commerce-722w.onrender.com/auth/google/callback",
+    callbackURL:"https://e-commerce-722w.onrender.com/auth/google/callback"||"http://localhost:4000/auth/google/callback",
     
   },
   async function(accessToken, refreshToken, profile, cb) {
